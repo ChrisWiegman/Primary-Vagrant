9affcaa7ea95899647150561295b29148a3c87a9
Chris Wiegman <chris@wiegman.us>
2018-04-11 16:40:42 -0400

    Working on Apache config

--- provision/environments/production/manifests/04_apache.pp
+++ provision/environments/production/manifests/04_apache.pp
@@ -1,5 +1,5 @@
 class { 'apache':
-  require => Apt::Ppa['ppa:ondrej/php'],
+	require => Apt::Ppa['ppa:ondrej/php'],
 }
 
 class { 'apache::mod::rewrite': }
@@ -12,58 +12,51 @@ class { 'apache::mod::proxy_fcgi': }
 class { 'apache::mod::ssl': }
 
 apache::vhost { 'dashboard.pv':
-  serveraliases => 'pv.io pv',
-  docroot       => '/var/www/default-sites/dashboard',
+	serveraliases => 'pv.io pv',
+	docroot       => '/var/www/default-sites/dashboard',
 }
 
 apache::vhost { 'phpmyadmin.pv':
-  docroot                  => '/var/www/default-sites/phpmyadmin/phpmyadmin',
-  directory                => '/var/www/default-sites/phpmyadmin/phpmyadmin',
-  directory_allow_override => 'All',
-  ssl                      => true,
+	docroot => '/var/www/default-sites/phpmyadmin/phpmyadmin',
+	options => ['override'],
+	ssl     => true,
 }
 
 apache::vhost { 'replacedb.pv':
-  docroot                  => '/var/www/internal-sites/replacedb',
-  directory                => '/var/www/internal-sites/replacedb',
-  directory_allow_override => 'All',
-  ssl                      => true,
+	docroot => '/var/www/internal-sites/replacedb',
+	options => ['override'],
+	ssl     => true,
 }
 
 apache::vhost { 'core.wordpress.pv':
-  docroot                  => '/var/www/default-sites/wordpress/core/wordpress/src',
-  directory                => '/var/www/default-sites/wordpress/core/wordpress/src',
-  directory_allow_override => 'All',
-  ssl                      => true,
+	docroot => '/var/www/default-sites/wordpress/core/wordpress/src',
+	options => ['override'],
+	ssl     => true,
 }
 
 apache::vhost { 'legacy.wordpress.pv':
-  docroot                  => '/var/www/default-sites/wordpress/legacy/htdocs',
-  directory                => '/var/www/default-sites/wordpress/legacy/htdocs',
-  aliases                  => '/content /var/www/default-sites/wordpress/content',
-  directory_allow_override => 'All',
-  ssl                      => true,
+	docroot => '/var/www/default-sites/wordpress/legacy/htdocs',
+	options => ['override'],
+	aliases => '/content /var/www/default-sites/wordpress/content',
+	ssl     => true,
 }
 
 apache::vhost { 'stable.wordpress.pv':
-  docroot                  => '/var/www/default-sites/wordpress/stable/htdocs',
-  directory                => '/var/www/default-sites/wordpress/stable/htdocs',
-  aliases                  => '/content /var/www/default-sites/wordpress/content',
-  directory_allow_override => 'All',
-  ssl                      => true,
+	docroot => '/var/www/default-sites/wordpress/stable/htdocs',
+	options => ['override'],
+	aliases => '/content /var/www/default-sites/wordpress/content',
+	ssl     => true,
 }
 
 apache::vhost { 'trunk.wordpress.pv':
-  docroot                  => '/var/www/default-sites/wordpress/trunk/htdocs',
-  directory                => '/var/www/default-sites/wordpress/trunk/htdocs',
-  aliases                  => '/content /var/www/default-sites/wordpress/content',
-  directory_allow_override => 'All',
-  ssl                      => true,
+	docroot => '/var/www/default-sites/wordpress/trunk/htdocs',
+	options => ['override'],
+	aliases => '/content /var/www/default-sites/wordpress/content',
+	ssl     => true,
 }
 
 apache::vhost { 'webgrind.pv':
-  docroot                  => '/var/www/internal-sites/webgrind',
-  directory                => '/var/www/internal-sites/webgrind',
-  directory_allow_override => 'All',
-  ssl                      => true,
+	docroot => '/var/www/internal-sites/webgrind',
+	options => ['override'],
+	ssl     => true,
 }
